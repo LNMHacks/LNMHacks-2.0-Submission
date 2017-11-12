b87f89063e7b6432e2b0790e9bdf411862e78230
Vishwesh Jainkuniya <gitvishwesh@gmail.com>
2017-11-12 09:01:42 +0530

    Completed visiting card and poster text extraction.

--- app/src/main/java/com/example/vishwesh/optio/OCR/OCRActivity.java
+++ app/src/main/java/com/example/vishwesh/optio/OCR/OCRActivity.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.provider.ContactsContract;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
@@ -29,6 +30,7 @@ import com.google.android.gms.vision.text.TextRecognizer;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.regex.Pattern;
 
 import static java.lang.Thread.sleep;
@@ -64,19 +66,58 @@ public class OCRActivity extends AppCompatActivity {
                 ArrayList<String> site = new ArrayList<String>();
                 ArrayList<String> email = new ArrayList<String>();
                 ArrayList<String> mobile = new ArrayList<String>();
-                String name = "", jobTitle = "";
+                String name = "", jobTitle = "", company = "";
+                String title = "", date = "", time = "", venue = "", twenty4 = "";
 
                 String wholeText = textBlockContent.getText().toString();
                 String[] splittedText = wholeText.split("\n");
                 for (int i = 0; i < splittedText.length; i++) {
-                    String[] split = splittedText[i].split(" ");
-                    for (int j = 0; j < split.length; j++) {
-                        if (urlPattern.matcher(split[j]).matches()) {
-                            site.add(split[j]);
-                        } else if (split[j].startsWith("+91")) {
-                            mobile.add(split[j]);
-                        } else if (pattern.matcher(split[j]).matches()) {
-                            email.add(split[j]);
+                    if (splittedText[i].contains(",")) {
+                        String[] split = splittedText[i].split(",");
+                        name = split[0];
+                        jobTitle = split[1];
+                    } else {
+                        //for poster
+                        if (splittedText[i].toLowerCase().startsWith("date")) {
+                            date = splittedText[i].split(" ")[1];
+                        } else if (splittedText[i].toLowerCase().startsWith("time")) {
+                            String[] strings = splittedText[i].split(" ");
+                            time = strings[1];
+                            twenty4 = strings.length > 2 ? strings[2] : "";
+                        } else if (splittedText[i].toLowerCase().startsWith("venue")) {
+                            venue = splittedText[i].split(" ")[1];
+                        } else {
+                            title = splittedText[i];
+                        }
+
+                        String[] split = splittedText[i].split(" ");
+                        boolean designationFlag = splittedText[i].toLowerCase().contains("designation");
+                        boolean imFlag = splittedText[i].toLowerCase().contains("i am");
+                        boolean companyFlag = splittedText[i].toLowerCase().contains("company");
+                        for (int j = 0; j < split.length; j++) {
+                            if (urlPattern.matcher(split[j]).matches()) {
+                                site.add(split[j]);
+                            } else if (split[j].startsWith("+91")) {
+                                mobile.add(split[j]);
+                            } else if (pattern.matcher(split[j]).matches()) {
+                                email.add(split[j]);
+                            } else if ((split[j].startsWith("\"") && split[j].endsWith("\""))
+                                    || (split[j].startsWith("|") && split[j].endsWith("|"))
+                                    || (split[j].startsWith("*") && split[j].endsWith("*"))
+                                    ) {
+                                company = split[j].substring(1, split[j].length() - 1);
+                            } else if (j != 0 && (designationFlag && !split[j].contains(":"))
+                                    || (imFlag && !split[j].toLowerCase().equals("i") && !split[j].toLowerCase().equals("am"))) {
+                                jobTitle += split[j];
+                            } else if (j != 0 && companyFlag && !split[j].contains(":")) {
+                                company += split[j] + " ";
+                            } else if (!split[j].toLowerCase().contains("company")
+                                    && !split[j].toLowerCase().contains("designation")
+                                    && !split[j].toLowerCase().equals("i")
+                                    && !split[j].toLowerCase().equals("am")
+                                    ) {
+                                name += split[j] + " ";
+                            }
                         }
                     }
                 }
@@ -109,10 +150,35 @@ public class OCRActivity extends AppCompatActivity {
                     }
                 } else if (type == 2) {
                     //scan contact
-                    //find phone number
+                    addAsContactConfirmed(OCRActivity.this,
+                            name,
+                            mobile.size() > 0 ? mobile.get(0) : "",
+                            email.size() > 0 ? email.get(0) : "",
+                            jobTitle + ", " + company
+                    );
 
                 } else if (type == 3) {
                     //poster
+                    String[] dateArray = date.split("/");
+                    String[] timeArray = time.split(":");
+                    Calendar c = Calendar.getInstance();
+                    c.set(Integer.parseInt(dateArray[2]), Integer.parseInt(dateArray[1]) - 1, Integer.parseInt(dateArray[0]));
+                    int hour = Integer.parseInt(timeArray[0]);
+                    int minute = Integer.parseInt(timeArray[1]);
+                    c.set(Calendar.MINUTE, minute);
+                    String s1 = twenty4;
+                    if (s1.toLowerCase().contains("am") || s1.toLowerCase().contains("pm")) {
+                        c.set(Calendar.HOUR, hour);
+                        if (s1.toLowerCase().contains("am")) {
+                            c.set(Calendar.AM_PM, 0);
+                        } else {
+                            c.set(Calendar.AM_PM, 1);
+                        }
+                    } else {
+                        c.set(Calendar.HOUR_OF_DAY, hour);
+                    }
+
+                    sendCalendarIntent(c.getTimeInMillis(), c.getTimeInMillis() + 100, title, venue);
                 }
                 ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
                 ClipData clip = ClipData.newPlainText("data", wholeText);
@@ -214,7 +280,11 @@ public class OCRActivity extends AppCompatActivity {
         cameraSource.release();
     }
 
-    public static void addAsContactConfirmed(final Context context, String name, String mobile, String email) {
+    public static void addAsContactConfirmed(final Context context,
+                                             String name,
+                                             String mobile,
+                                             String email,
+                                             String jobTitle) {
 
         Intent intent = new Intent(Intent.ACTION_INSERT);
         intent.setType(ContactsContract.Contacts.CONTENT_TYPE);
@@ -222,6 +292,7 @@ public class OCRActivity extends AppCompatActivity {
         intent.putExtra(ContactsContract.Intents.Insert.NAME, name);
         intent.putExtra(ContactsContract.Intents.Insert.PHONE, mobile);
         intent.putExtra(ContactsContract.Intents.Insert.EMAIL, email);
+        intent.putExtra(ContactsContract.Intents.Insert.COMPANY, jobTitle);
 
         context.startActivity(intent);
 
@@ -249,38 +320,54 @@ public class OCRActivity extends AppCompatActivity {
         AlertDialog alert = builder.create();
         alert.show();
     }
- public CharSequence[] toCharSeq(ArrayList<String> a){
-        CharSequence [] c=new CharSequence[a.size()];
-        for(int i=0;i<a.size();i++){
-            c[i]=a.get(i);
+
+    public CharSequence[] toCharSeq(ArrayList<String> a) {
+        CharSequence[] c = new CharSequence[a.size()];
+        for (int i = 0; i < a.size(); i++) {
+            c[i] = a.get(i);
         }
 
         return c;
 
 
- }
- public void openBrowser(String url){
-     Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-     startActivity(browserIntent);
- }
+    }
 
- public void openEmail(String email)
- {
-     Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
-     emailIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-     emailIntent.setType("vnd.android.cursor.item/email");
-     emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[] {email});
-     //emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "My Email Subject");
-     //emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, "My email content");
-     startActivity(Intent.createChooser(emailIntent, "Send mail using..."));
- }
+    public void openBrowser(String url) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        startActivity(browserIntent);
+    }
 
- public void openDialer(String phone){
-     Intent intent = new Intent(Intent.ACTION_DIAL);
-     intent.setData(Uri.parse("tel:"+phone));
-     startActivity(intent);
- }
+    public void openEmail(String email) {
+        Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+        emailIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        emailIntent.setType("vnd.android.cursor.item/email");
+        emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{email});
+        //emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "My Email Subject");
+        //emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, "My email content");
+        startActivity(Intent.createChooser(emailIntent, "Send mail using..."));
+    }
 
+    public void openDialer(String phone) {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + phone));
+        startActivity(intent);
+    }
+
+    private void sendCalendarIntent(long startTime, long endTime, String result, String venue) {
+        Intent intent = new Intent(Intent.ACTION_INSERT)
+                .setData(CalendarContract.Events.CONTENT_URI)
+                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+                        startTime)
+                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
+                        endTime)
+                .putExtra(CalendarContract.Events.TITLE, result)
+                //.putExtra(CalendarContract.Events.DESCRIPTION, result)
+                .putExtra(CalendarContract.Events.EVENT_LOCATION, venue)
+                //.putExtra(CalendarContract.Events.AVAILABILITY, CalendarContract.Events.AVAILABILITY_BUSY)
+                // .putExtra(Intent.EXTRA_EMAIL, "rowan@example.com,trevor@example.com")
+                ;
+        startActivity(intent);
+    }
 }
 
 
